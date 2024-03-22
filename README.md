ec16b99b3ca4195714a366050ade6935848f7217
Bart Roos <bartjroos@gmail.com>
2024-03-22 16:30:23 +0100

    WeekResource

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Weeks
 
-**STATUS 15-03:**
+**STATUS 22-03:**
 Met de Wizard kun je nu een heel schooljaar aanmaken.
 
 Todo:
-* Weken resource in linkermenu net zoals schooljaren en semester
+* ~~Weken resource in linkermenu net zoals schooljaren en semester~~
+* WeekResource form opbouwen
 * In de wizard(?) een manier om voor aparte cohorten ook aparte weken/semesters aan te duiden
---
+++ app/Filament/Resources/WeekResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\WeekResource\Pages;
+use App\Filament\Resources\WeekResource\RelationManagers;
+use App\Models\Week;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class WeekResource extends Resource
+{
+    protected static ?string $model = Week::class;
+    protected static ?string $navigationGroup = 'Basisdata';
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('semester.naam'),
+                Tables\Columns\TextColumn::make('nummer')
+                    ->numeric(),
+                Tables\Columns\TextColumn::make('maandag')
+                    ->date(),
+                Tables\Columns\TextColumn::make('type'),
+                Tables\Columns\TextColumn::make('naam'),
+                Tables\Columns\TextColumn::make('cohort')
+                    ->numeric(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListWeeks::route('/'),
+            'create' => Pages\CreateWeek::route('/create'),
+            'edit' => Pages\EditWeek::route('/{record}/edit'),
+        ];
+    }
+}
