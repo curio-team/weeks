c3b587621e1f8fd7b07a0e29ae5c4a56b5b95817
luttje <2738114+luttje@users.noreply.github.com>
2025-04-04 09:20:18 +0200

    Allow week offset during development

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,16 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
+
+        $offsetWeeks = config('app.dev.offset_weeks');
+
+        if ($offsetWeeks) {
+            $now = now()->addWeeks($offsetWeeks);
+
+            // Set this as the current time in the application
+            // This will affect all date/time operations
+            // throughout the application
+            Carbon::setTestNow($now);
+        }
     }
 }
